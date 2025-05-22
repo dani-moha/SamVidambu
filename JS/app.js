6428c0980635030a191585ef56b27e7091a32125
Witty Banter <danimoreloza@gmail.com>
2025-05-23 02:31:58 +0300

    Delete JS directory

--- JS/app.js
+++
@@ -1,9 +0,0 @@
-
-function toggleReadMore(button) {
-  const moreText = button.parentElement.querySelector(".more-text");
-  const isVisible = moreText.style.display === "block";
-
-  moreText.style.display = isVisible ? "none" : "block";
-  button.textContent = isVisible ? "Read More" : "Read Less";
-}
-
